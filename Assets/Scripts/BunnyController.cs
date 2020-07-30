1bda4b0bb5966c509942dc142c66ba80329dcc45
Luke Warren <warrenpluke@gmail.com>
2020-07-30 16:15:11 +1200

    Tutorial 7
    Death animation and basic score system implemented.

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour
 {
     private Rigidbody2D myRigidBody;
+    private Collider2D myCollider;
     public float bunnyJumpForce = 500f;
     private Animator myAnim;
     string currentScene;
+    private float bunnyHurtTime = -1;
+    public Text scoreText;
+    private float startTime;
 
     // Start is called before the first frame update
     void Start()
@@ -16,30 +21,53 @@ public class BunnyController : MonoBehaviour
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
         currentScene = SceneManager.GetActiveScene().name;
+        myCollider = GetComponent<Collider2D>();
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonUp("Jump"))
+        if (bunnyHurtTime == -1) 
         {
-            myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            if(Input.GetButtonUp("Jump"))
+            {
+                myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            }
+            myAnim.SetFloat("Velocity",System.Math.Abs(myRigidBody.velocity.y));
+            scoreText.text = (Time.time - startTime).ToString("0.0");
         }
-        myAnim.SetFloat("Velocity",System.Math.Abs(myRigidBody.velocity.y));
+        else
+        {
+            if(Time.time > bunnyHurtTime + 2)
+            {
+                SceneManager.LoadScene(currentScene);
+                //Application.LoadLevel has been depreciated from the time of the tutorial.
+                //Replaced with SceneManager.
+                //Application.LoadLevel(Application.loadedLevel);
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         // https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnCollisionEnter2D.html
-       if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+       if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy") )
        {
-           string currentScene = SceneManager.GetActiveScene().name;
-           SceneManager.LoadScene(currentScene);
+           PrefabSpawner spawner = FindObjectOfType<PrefabSpawner>();
+           spawner.enabled = false;
+           foreach (MoveLeft movelefter in FindObjectsOfType<MoveLeft>())
+           {
+               movelefter.enabled = false;
+           }
+           bunnyHurtTime = Time.time;
+           myAnim.SetBool("Bunny Hurt", true);
+           myRigidBody.velocity = Vector2.zero;
+           myRigidBody.AddForce(transform.up * bunnyJumpForce);
+           myCollider.enabled = false;
        }
         
-        //Application.LoadLevel has been made redundant from the time of the tutorial.
-        //Replaced with SceneManager. Functionality appears identical.
-        //Application.LoadLevel(Application.loadedLevel);
+        
 
 
     }
